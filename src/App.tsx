effcb4fb8114ce34026fa1a9def29b2df2fa774c
jamesdanielwhitford <james.whitford0@gmail.com>
2025-12-07 07:46:34 +0200

    Initial app commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Offline-First App</h1>
+      <p>Ready to build</p>
+    </div>
+  )
+}
+
+export default App
